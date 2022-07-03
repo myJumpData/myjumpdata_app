1a667659590e284354607c8e0ae4838e9a16f982
Marko Fediv <marko.fediv@gmail.com>
2022-07-03 13:34:16 +0200

    removed debug info

--- src/app/screens/LoginScreen.tsx
+++ src/app/screens/LoginScreen.tsx
@@ -1,30 +1,25 @@
-import * as React from "react";
-import { useTranslation } from "react-i18next";
-import { Image, View } from "react-native";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import Logo from "../assets/Logo.png";
-import { StyledButton } from "../components/StyledButton";
-import {
-  StyledShyText,
-  StyledText,
-  StyledTextH1,
-} from "../components/StyledText";
-import { StyledTextInput } from "../components/StyledTextInput";
-import { StyledView } from "../components/StyledView";
-import { Colors } from "../Constants";
-import AuthService from "../services/auth.service";
-import getApi from "../utils/getApi";
+import * as React from 'react';
+import {useTranslation} from 'react-i18next';
+import {Image, View} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import Logo from '../assets/Logo.png';
+import {StyledButton} from '../components/StyledButton';
+import {StyledText, StyledTextH1} from '../components/StyledText';
+import {StyledTextInput} from '../components/StyledTextInput';
+import {StyledView} from '../components/StyledView';
+import {Colors} from '../Constants';
+import AuthService from '../services/auth.service';
 
-export default function LoginScreen({ navigation }) {
-  const { t } = useTranslation();
-  const [username, onChangeUsername] = React.useState("");
-  const [password, onChangePassword] = React.useState("");
+export default function LoginScreen({navigation}) {
+  const {t} = useTranslation();
+  const [username, onChangeUsername] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
 
   function handleLoginSubmit() {
-    AuthService.login(username.trim(), password).then((res) => {
+    AuthService.login(username.trim(), password).then(res => {
       if (res.data) {
-        onChangePassword("");
-        onChangeUsername("");
+        onChangePassword('');
+        onChangeUsername('');
       }
     });
   }
@@ -33,44 +28,38 @@ export default function LoginScreen({ navigation }) {
     <StyledView
       style={{
         flex: 1,
-        justifyContent: "space-between",
-      }}
-    >
+        justifyContent: 'space-between',
+      }}>
       <ScrollView
         style={{
           padding: 10,
           flex: 1,
-        }}
-      >
+        }}>
         <View
           style={{
             flex: 1,
-            alignItems: "center",
-            justifyContent: "space-between",
-            flexDirection: "row",
-          }}
-        >
-          <StyledText style={{ fontWeight: "900", fontSize: 28, padding: 5 }}>
-            {t("common:nav_login")}
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+          }}>
+          <StyledText style={{fontWeight: '900', fontSize: 28, padding: 5}}>
+            {t('common:nav_login')}
           </StyledText>
           <TouchableOpacity
             onPress={() => {
-              navigation.navigate("register");
-            }}
-          >
+              navigation.navigate('register');
+            }}>
             <StyledText
-              style={{ color: Colors.main, fontWeight: "900", padding: 5 }}
-            >
-              {t("common:nav_signup")}
+              style={{color: Colors.main, fontWeight: '900', padding: 5}}>
+              {t('common:nav_signup')}
             </StyledText>
           </TouchableOpacity>
         </View>
         <StyledView
           style={{
-            alignItems: "center",
-            height: "30%",
-          }}
-        >
+            alignItems: 'center',
+            height: '30%',
+          }}>
           <Image
             source={Logo}
             style={{
@@ -81,17 +70,15 @@ export default function LoginScreen({ navigation }) {
         </StyledView>
         <StyledView
           style={{
-            alignItems: "center",
-          }}
-        >
+            alignItems: 'center',
+          }}>
           <StyledTextH1>myJumpData</StyledTextH1>
         </StyledView>
         <StyledView
           style={{
             paddingVertical: 10,
-          }}
-        >
-          <StyledText>{t("common:username")}:</StyledText>
+          }}>
+          <StyledText>{t('common:username')}:</StyledText>
           <StyledTextInput
             testId="login_username"
             onChangeText={onChangeUsername}
@@ -105,9 +92,8 @@ export default function LoginScreen({ navigation }) {
         <StyledView
           style={{
             paddingVertical: 10,
-          }}
-        >
-          <StyledText>{t("common:password")}:</StyledText>
+          }}>
+          <StyledText>{t('common:password')}:</StyledText>
           <StyledTextInput
             testId="login_password"
             onChangeText={onChangePassword}
@@ -122,22 +108,13 @@ export default function LoginScreen({ navigation }) {
         <StyledView
           style={{
             paddingVertical: 10,
-          }}
-        >
+          }}>
           <StyledButton
-            title={t("common:nav_login")}
+            title={t('common:nav_login')}
             onPress={handleLoginSubmit}
           />
         </StyledView>
       </ScrollView>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <StyledShyText>{getApi()}</StyledShyText>
-      </View>
     </StyledView>
   );
 }
